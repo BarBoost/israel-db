7234675ccf7f4a83c885d59d210e0059666baaf2
BarBoost <113690127+BarBoost@users.noreply.github.com>
2023-10-19 13:43:26 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
-            document.onreadystatechange = function() {
-                "complete" !== document.readyState ? (document.querySelector(".airtable-embed-content").style.visibility = "hidden",
-                document.querySelector("#loader").style.visibility = "visible") : (document.querySelector("#loader").style.display = "none",
-                document.querySelector(".airtable-embed-content").style.visibility = "visible")
-            }
-            ;
+document.onreadystatechange = function() {
+    "complete" !== document.readyState ? (document.querySelector(".airtable-embed-content").style.visibility = "hidden",
+        document.querySelector("#loader").style.visibility = "visible") : (document.querySelector("#loader").style.display = "none",
+        document.querySelector(".airtable-embed-content").style.visibility = "visible")
+};
